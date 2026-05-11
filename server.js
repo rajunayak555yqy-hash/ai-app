c577409c5609147948ec61ec1af8552273ea539f
rajunayak555yqy-hash <rajunayak555yqy@gmail.com>
2026-05-12 04:18:52 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,11 +1,18 @@
 let orders = [];
 import express from "express";
 import cors from "cors";
+import path from "path";
+import { fileURLToPath } from "url";
 
 const app = express();
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 app.use(cors());
 app.use(express.json());
+app.use(express.static(__dirname));
+
 
 const menu = [
   { name: "Burger", price: 50 },
